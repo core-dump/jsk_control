1a2b04c3adca0be2174e2e1ba63646585f3b9108
Satoshi Iwaishi <iwaishi@jsk.imi.i.u-tokyo.ac.jp>
2015-09-08 14:29:57 +0900

    変換を実装

--- jsk_teleop_joy/scripts/midi_relative_converter.py
+++ jsk_teleop_joy/scripts/midi_relative_converter.py
@@ -15,29 +15,49 @@ except:
 
 class AxesConverter:
   def __init__(self, axes):
-    self.input_origin = list(axes)
-    self.output_origin = [0.5] * len(axes)
+    self.input_origins = [None] * len(axes)
+    self.output_origins = [0.5] * len(axes)
 
   def convert(self, abs_axes):
-    rel_axes = [0] * len(abs_axes)
+    self.__updateInputOrigins(abs_axes)
+    return self.__convertAxes(abs_axes, self.input_origins, self.output_origins)
+
+  def __updateInputOrigins(self, abs_axes):
     for i in range(len(abs_axes)):
-      rel_axes[i] = abs_axes[i]
+      if self.input_origins[i] is None and abs_axes[i] != 0 and abs_axes[i] != 1:
+        self.input_origins[i] = abs_axes[i]
+
+  def __convertAxes(self, abs_axes, in_origins, out_origins):
+    rel_axes = [None] * len(abs_axes)
+    for i in range(len(abs_axes)):
+      rel_axes[i] = self.__convertAxis(abs_axes[i], in_origins[i], out_origins[i])
     return rel_axes
 
+  def __convertAxis(self, abs_axis, in_origin, out_origin):
+    if in_origin is None:
+      rel_axis = out_origin
+    elif abs_axis < in_origin:
+      rel_axis = (((abs_axis - 0.0) /  (in_origin - 0.0)) * (out_origin - 0.0)) + 0.0
+    elif abs_axis == in_origin:
+      rel_axis = out_origin
+    elif abs_axis > in_origin:
+      rel_axis = (((abs_axis - in_origin) /  (1.0 - in_origin)) * (1.0 - out_origin)) + out_origin
+    return rel_axis
+
 def joy_callback(data):
-  joy_pub = rospy.Publisher("/joy_relative", Joy)
-  joy = Joy()
+  global ac
   try:
     ac
   except NameError:
     ac = AxesConverter(data.axes)
+  joy_pub = rospy.Publisher("/joy_relative", Joy)
+  joy = Joy()
   joy.header.stamp = rospy.Time.now()
   joy.axes = ac.convert(data.axes)
   joy.buttons = data.buttons
   joy_pub.publish(joy)
 
 def main():
-  global ac
   rospy.init_node('midi_joy')
   rospy.Subscriber("/joy", Joy, joy_callback)
   rospy.spin()

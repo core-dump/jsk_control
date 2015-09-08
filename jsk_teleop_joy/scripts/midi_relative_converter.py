565d38a5a4877f4afe05ef2b3499fcccadc190f2
Satoshi Iwaishi <iwaishi@jsk.imi.i.u-tokyo.ac.jp>
2015-09-08 14:29:53 +0900

    変換用クラスを準備

--- jsk_teleop_joy/scripts/midi_relative_converter.py
+++ jsk_teleop_joy/scripts/midi_relative_converter.py
@@ -13,15 +13,31 @@ except:
   roslib.load_manifest('jsk_teleop_joy')
   from sensor_msgs.msg import Joy, JoyFeedbackArray
 
+class AxesConverter:
+  def __init__(self, axes):
+    self.input_origin = list(axes)
+    self.output_origin = [0.5] * len(axes)
+
+  def convert(self, abs_axes):
+    rel_axes = [0] * len(abs_axes)
+    for i in range(len(abs_axes)):
+      rel_axes[i] = abs_axes[i]
+    return rel_axes
+
 def joy_callback(data):
   joy_pub = rospy.Publisher("/joy_relative", Joy)
   joy = Joy()
+  try:
+    ac
+  except NameError:
+    ac = AxesConverter(data.axes)
   joy.header.stamp = rospy.Time.now()
-  joy.axes = data.axes
-  joy.buttons = joy.buttons
+  joy.axes = ac.convert(data.axes)
+  joy.buttons = data.buttons
   joy_pub.publish(joy)
 
 def main():
+  global ac
   rospy.init_node('midi_joy')
   rospy.Subscriber("/joy", Joy, joy_callback)
   rospy.spin()
